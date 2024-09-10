35b40dac60c8dc0e3998eb6f341fb3c772d6eda6
Leandro Gómez <leandrodanielgomez@gmail.com>
2024-09-10 16:39:32 -0300

    feat: added method to take a card from the stack

--- 2024/clase 09-04/src/cartas/Main.java
+++ 2024/clase 09-04/src/cartas/Main.java
@@ -1,13 +1,15 @@
 package cartas;
 
-import java.util.Arrays;
+/**
+ * Hacer un juego de cartas en el que, al pedir una carta, si se saca del mazo,
+ * no se pueda volver a sacar una igual.
+ * Funcionalidades:
+ * 1) Pedir cartas
+ * 2) Comparar
+ */
 
 public class Main {
     public static void main(String[] args) {
-        Carta [] mazo = new Carta().generarMazo();
-
-        for (Carta carta : mazo) {
-            System.out.println(carta.cartaToString());
-        }
+        Mazo mazo = new Mazo();
     }
 }
--- 2024/clase 09-04/src/cartas/Palo.java
+++ 2024/clase 09-04/src/cartas/Palo.java
@@ -1,14 +1,5 @@
 package cartas;
 
 public enum Palo {
-    COPA("Copa"),
-    ESPADA("Basto"),
-    BASTO("Basto"),
-    ORO("Oro");
-
-    public final String nombre;
-
-    Palo(String nombre) {
-        this.nombre = nombre;
-    }
+    COPA, ESPADA, BASTO, ORO;
 }
